9afe58ca294e52abb21e12c83374e3c5e1cf41c4
vanderlinde <marcelojr.vanderlinde@gmail.com>
2019-12-06 14:18:03 -0400

    PROVA do Rodrigo Paiva - Programação Mobile

---
+++ PROVA/PROVA/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PROVA
+{
+    public partial class App : Application
+    {
+        public static MasterDetailPage MasterPage { get; set; }
+
+        public static async Task NavigateMaster(Page page)
+        {
+            App.MasterPage.IsPresented = false;
+            await App.MasterPage.Detail.Navigation.PushAsync(page);
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MasterDetailPage1();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ PROVA/PROVA/MasterDetailPage1.xaml.cs
@@ -0,0 +1,50 @@
+﻿using PROVA.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PROVA
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterDetailPage1 : MasterDetailPage
+    {
+        public MasterDetailPage1()
+        {
+            InitializeComponent();
+            MasterPage.ListView.ItemSelected += ListView_ItemSelected;
+        }
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as MasterDetailPage1MasterMenuItem;
+            if (item == null)
+                return;
+            
+
+            var page = new Page();
+            if (item.Id == 0)
+            {
+                page = new View.testeQuemSou();
+            }if (item.Id == 1)
+            {
+                page = new View.testeSite();
+            }if (item.Id == 2)
+            {
+                page = new View.testeDisciplina();
+            }
+
+
+            page.Title = item.Title;
+
+            Detail = new NavigationPage(page);
+            IsPresented = false;
+
+            MasterPage.ListView.SelectedItem = null;
+        }
+    }
+}
